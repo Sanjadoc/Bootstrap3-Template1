ef3c45129ab9a3384488779afbfed9916d7b0d3a
sanjadoc <sanja_doc@ukr.net>
2015-12-10 15:31:50 +0200

    BS3T1-6-add Features

---
+++ js/scripts.js
@@ -0,0 +1,18 @@
+/*
+
+ My Custom JS
+ =============
+
+ Author:  ...
+ Updated: December 2015
+ Notes:	 For my Education
+
+ */
+
+$(function(){
+    $('#alertMe').click(function(e){
+        e.preventDefault();
+
+        $('#successAlert').slideDown();
+    });
+});
